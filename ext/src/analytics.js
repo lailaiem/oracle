805c407ef685a946cd266500f9c1c9469d2b5607
lailai <lailaiem@users.noreply.github.com>
2017-04-12 18:02:13 +1000

    fix duplicate i0_unset analytics events

--- ext/src/analytics.js
+++ ext/src/analytics.js
@@ -4,6 +4,7 @@ let UNIQUE_ID = 'i0_unset';
 function trackAnalyticsEvent(eventName, eventProps) {
 	if (UNIQUE_ID === "i0_unset") {
 		setTimeout(() => trackAnalyticsEvent(eventName, eventProps), 100);
+		return;
 	}
 	// 45.63.17.67 = epicmafia.net
 	$("body").append(`<img src="http://45.63.17.67/oracle/a.php?u=${currentUser}&i=${UNIQUE_ID}&n=${eventName}&p=${btoa(JSON.stringify(eventProps))}" height="0" width="0" />`);
